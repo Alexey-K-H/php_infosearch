ddadcaaeb1e5806956e42e6a944a28431655b35e
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2021-10-21 09:31:32 +0700

    add files fro labs

---
+++ test1/z2/z2-3.php
@@ -0,0 +1,17 @@
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title> z2-3 </title>
+</head>
+<body>
+<?php
+$breakfast = "hamburger";
+$breakfast2 = &$breakfast;
+
+print $breakfast2."<br>\n";
+
+$breakfast = "tea";
+print $breakfast2;
+?>
+</body>
+</html>
