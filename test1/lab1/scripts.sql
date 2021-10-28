3b4cb9d73cae771c221fd69d9ae99c82a0840de5
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2021-10-28 16:08:00 +0700

    fixes

--- test1/lab1/scripts.sql
+++ test1/lab1/scripts.sql
@@ -12,28 +12,28 @@ select sname, city from sal where city='London' and comm > 0.11;
 select * from cust where rating<=200 and city not like 'Rome';
 #7
 select * from ord where odate='03-OCT-90' or odate='05-OCT-90';
-#8
+
 select * from ord where odate!='04-OCT-90' and odate!='06-OCT-90';
-#9
+#8
 select * from cust where cname rlike '^[A-G]';
-#10
+#9
 select * from sal where sname like '%e%';
-#11
+#10
 select SUM(amt) from ord where odate='03-OCT-90';
-#12
+#11
 select SUM(amt) from ord where snum=1001;
-#13
+#12
 select MAX(amt), snum from ord group by snum;
-#14
+#13
 select MIN(cname) as name from cust where cname like '%s';
-#15
+#14
 select AVG(comm), city from sal group by city;
-#16
+#15
 select onum, amt * 0.8 as awtEUR, s.sname, s.comm
 from ord
          inner join sal s on ord.snum = s.snum
 where odate = '03-OCT-90';
-#17
+#16
 select o.onum, ocs.cname, ocs.sname, ocs.city
 from ord o
          left join
@@ -45,21 +45,21 @@ from ord o
          or 'Rome'
      ) as ocs on o.onum = ocs.onum
 order by o.onum;
-#18
+#17
 select s.sname, SUM(o.amt) as sincome, SUM(o.amt) * s.comm as scomm_sum
 from sal as s
          inner join ord as o on s.snum = o.snum
 where o.odate < '05-OCT-90'
 group by s.sname
 order by s.sname;
-#19
+#18
 select o.onum, o.amt, c.cname, c.city, s.sname, s.city
 from ord as o
          inner join cust c on o.cnum = c.cnum
          inner join sal s on c.snum = s.snum
 where c.city rlike '^[L-R]'
   and s.city rlike '^[L-R]';
-#20
+#19
 with ctw1 as
          (select o.onum as first, o2.onum as second, c.cname as cname_1, c2.cname as cname_2
           from ord o
@@ -70,7 +70,7 @@ with ctw1 as
             and c.cname != c2.cname)
 select distinct ctw1.cname_1, ctw1.cname_2
 from ctw1;
-#21
+#20
 with t1 as (
     /*Продавцы с комиссионными  меньше 0.13*/
     select s.snum, s.comm
@@ -87,12 +87,12 @@ select t2.cname, t1.snum
 from t2
          inner join t1 on t2.snum = t1.snum;
 
-#22
+#21
 create table sal_copy
 as select * from sal;
 desc sal;
 desc sal_copy;
-#23
+#22
 insert into sal_copy
 values (1005, 'James', 'Rome', 0.2);
 
@@ -104,7 +104,7 @@ select * from sal_copy;
 delete from sal_copy where snum = 1005;
 
 select * from sal_copy;
-
+#23
 insert into sal_copy
 values (1008, 'Mike', 'New York', 0.06);
 
