8a042ff47f0c11f77037c007d40606d44ce7c12a
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2021-10-28 16:35:04 +0700

    fixes

--- test1/lab3/z4-1b.php
+++ test1/lab3/z4-1b.php
@@ -27,7 +27,7 @@ if(!isset($valign)){
 if(isset($align) && isset($valign)){
     print "<table style='border-collapse: separate; table-layout: fixed'>\n";
     print "<tr>\n\t<td class='temp' style='text-align: $align; vertical-align: $valign;'>Текст</td>\n</tr>\n";
-    print "<tr>\n\t<td style='text-align: center;'><a href='lab3-1a.html'>Назад</a></td>\n</tr>\n";
+    print "<tr>\n\t<td style='text-align: center;'><a href='z4-1a.html'>Назад</a></td>\n</tr>\n";
     print "</table>";
 }
 ?>
